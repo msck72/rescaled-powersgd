e270b83f93738e7d668ee107608c1a5a8379e505
Chaitanya <chaitanya.mangipudi2002@gmail.com>
2025-04-29 15:55:21 +0530

    Initial code to test the quality of approximation of conventioanl powersgd and rescalaed powersgd

---
+++ powersgd/__init__.py
@@ -0,0 +1,29 @@
+import torch
+
+from powersgd.powersgd import Aggregator, AllReduce, Config, PowerSGD
+from powersgd.utils import params_in_optimizer
+
+
+def optimizer_step(optimizer: torch.optim.Optimizer, aggregator: Aggregator, type_of_aggregation):
+    """
+    Aggregate gradients across workers using `aggregator`,
+    and then take an optimizer step using the aggregated gradient.
+    """
+    params = params_in_optimizer(optimizer)
+    grads = [p.grad.data for p in params]  # type: ignore
+    
+    if type_of_aggregation == 'aggregate':
+        avg_grads = aggregator.aggregate(grads)  # subtracts the approximation from grads
+    else:
+        avg_grads = aggregator.aggregate_using_rescaled_grads(grads)  # subtracts the approximation from grads
+
+    # Temporarily set parameter's gradients to the aggregated values
+    for (p, g) in zip(params, avg_grads):
+        p.grad = g
+
+    # Run an optimizer step
+    optimizer.step()
+
+    # Put back the error buffer as the parameter's gradient
+    for (p, g) in zip(params, grads):
+        p.grad = g
